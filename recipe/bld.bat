517351385ff0d0eb41a67dfa349b9ad671a59aaf
H. Vetinari <h.vetinari@gmx.com>
2026-08-14 10:54:12 +1100

    break up long CMake invocation into several lines

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,7 +2,12 @@ set FFLAGS=-fno-range-check
 set CFLAGS=-DMS_WIN64
 REM cd %SRC_DIR%
 REM install.bat -DPython3_EXECUTABLE="%PYTHON%" -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="%PREFIX%" %CMAKE_ARGS%
-cmake -S "%SRC_DIR%" -B build -DPython3_EXECUTABLE="%PYTHON%" -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="%PREFIX%" %CMAKE_ARGS%
+cmake -S "%SRC_DIR%" -B build ^
+    -DPython3_EXECUTABLE="%PYTHON%" ^
+    -G "MinGW Makefiles" ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_INSTALL_PREFIX="%PREFIX%" ^
+    %CMAKE_ARGS%
 if errorlevel 1 exit 1
 cmake --build build --config Release --parallel %CPU_COUNT% --target install
 if errorlevel 1 exit 1
